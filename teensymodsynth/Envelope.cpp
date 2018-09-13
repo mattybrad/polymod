ff788a3386cbf7f8dac4b9b17470e47531658c46
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2018-09-13 16:26:36 +0100

    adsr envelope working!

--- teensymodsynth/Envelope.cpp
+++ teensymodsynth/Envelope.cpp
@@ -4,15 +4,20 @@
 
 Envelope::Envelope() {
   _dc.amplitude(1.0);
-  _envelope.attack(2000);
-  _envelope.release(3000);
+  _envelope.attack(100);
+  _envelope.release(2000);
   _patchCable1 = new AudioConnection(_dc,0,_envelope,0);
   _queue.begin();
-  sockets[0].setInput(_queue, 0); // gate
-  sockets[1].setOutput(_envelope, 0); // audio out
+  sockets[5].setInput(_queue, 0); // gate
+  sockets[2].setOutput(_envelope, 0); // audio out
+  // to do: add retrigger function on socket 1
 }
 
 void Envelope::update() {
+  _envelope.attack(1000*controls[4].getSmoothedValue());
+  _envelope.decay(1000*controls[6].getSmoothedValue());
+  _envelope.sustain(controls[7].getSmoothedValue());
+  _envelope.release(2000*controls[5].getSmoothedValue());
   if(_queue.available() > 0) {
     int16_t test[128];
     memcpy(test, _queue.readBuffer(), 256);
